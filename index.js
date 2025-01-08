d3e3dfa909bf3fd4de5522448de06bc4d15e6f27
bhavinSOL <bvmsolutions9913@gmail.com>
2025-01-08 10:17:32 +0530

    v 0.0.2

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const { Ollama } = require("ollama");
+// const { Ollama } = require("ollama");
 
-async function runChat() {
+// async function runChat() {
+//   try {
+//     const ollama = new Ollama();
+//     const response = await ollama.chat({
+//       model: "qwen2:0.5b",
+//       messages: [{ role: "user", content: "Write a bug report" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+// runChat();
+
+import ollama from "ollama";
+import fs from "fs";
+
+let q= fs.readFileSync("./q.txt", "utf-8");
+console.log(q)
+
+askQuestion()
+async function askQuestion() {
   try {
-    const ollama = new Ollama();
     const response = await ollama.chat({
       model: "qwen2:0.5b",
-      messages: [{ role: "user", content: "Generate marketing emails" }]
+      messages: [{ role: 'user', content: q }]
     });
 
-    console.log("Chatbot Response:", response.message.content);
+    fs.writeFileSync("./a.txt", response.message.content);
+
   } catch (error) {
     console.error("Error occurred:", error.message);
   }
 }
-
-runChat();
